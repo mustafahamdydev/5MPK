34f88a570df74099d2364a8153e0bce521510ad8
Mustafa Hamdy <mustafahamdy.010@gmail.com>
2024-04-18 13:24:08 +0200

    added about us and privacy policy and beta otp verify and add google sign in with user info

--- app/src/main/java/com/fivempk/activities/ChangePasswordActivity.kt
+++ app/src/main/java/com/fivempk/activities/ChangePasswordActivity.kt
@@ -12,10 +12,6 @@ class ChangePasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_change_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
